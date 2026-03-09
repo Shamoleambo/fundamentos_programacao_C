4fe4c07cf06ecea64f8b4919ce6a4400e9b69415
Tiago Gomes <tidgomes@gmail.com>
2026-03-09 13:03:02 -0300

    comecei ex07

---
+++ cp4/ex07/ex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Faça um programa que receba quatro valores: I, A, B e C. Desses valores I é inteiro e positivo, A, B e C são reais. Escreva os números A, B e C obedecendo a tabela a seguir.
+ * Suponha que o valor digitado para I seja sempre um valor válido, ou seja, 1, 2 ou 3, e que os números digitados sejam diferentes um do outro.
+ *
+ * VALOR DE I		FORMA A ESCREVER
+ * 1			A, B e C em ordem crescente.
+ * 2			A, B e C em ordem decrescente.
+ * 3			O maior fica entre os dois números.  */
+
+int main() {
+	return 0;
+}
